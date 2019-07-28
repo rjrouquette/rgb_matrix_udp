adacffad62fc455dca74a4cfb962b8384fc8c0e4
Robert J. Rouquette <robert.rouquette@focusvq.com>
2019-07-28 16:56:54 -0500

    visually enumerate display panels on startup

--- udp_transmitter/main.cpp
+++ udp_transmitter/main.cpp
@@ -4,6 +4,8 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <sysexits.h>
 #include <pthread.h>
 #include <csignal>
@@ -11,10 +13,13 @@
 #include <sys/time.h>
 #include <json-c/json.h>
 #include <sys/mman.h>
+#include <cerrno>
 
 #include "logging.h"
 
 #define UNUSED __attribute__((unused))
+
+uint64_t frameMagic = 0xebb29724dd5126acUL;
 
 struct frame_packet {
     uint32_t frameId;
@@ -49,13 +54,11 @@ long microtime();
 void sig_ignore(UNUSED int sig) { }
 
 bool loadConfiguration(const char *fileName);
-
-void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-    uint8_t *pixel = &(pixBuffAct[(y * width + x) * 3]);
-    pixel[0] = r;
-    pixel[1] = g;
-    pixel[2] = b;
-}
+void labelPanels();
+void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b);
+void drawHex(int xoff, int yoff, uint8_t value);
+void drawHex2(int xoff, int yoff, uint8_t value);
+void flipBuffer();
 
 int main(int argc, char **argv) {
     struct sigaction act = {};
@@ -81,11 +84,73 @@ int main(int argc, char **argv) {
     pixBuffAct = (uint8_t *) pixelBuffer;
     pixBuffNext = pixBuffAct + frameSize;
 
+    labelPanels();
+
     // start udp rx thread
     log("start udp tx thread");
     pthread_create(&threadUdpTx, nullptr, doUdpTx, nullptr);
 
-    pause();
+    const int sockListen = socket(PF_UNIX, SOCK_STREAM, 0);
+    struct sockaddr_un addr = {};
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, "rgb-matrix.sock", sizeof(addr.sun_path)-1);
+
+    unlink("rgb-matrix.sock");
+    if(bind(sockListen, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        log("ERROR on socket binding! %s (%d)", strerror(errno), errno);
+        log("failed to bind listening socket");
+
+        close(sockListen);
+        log("socket closed");
+
+        log("exiting");
+        return EX_CANTCREAT;
+    }
+    log("unix socket opened");
+
+    sockaddr clientAddr = {};
+    socklen_t addrLen;
+    const char *magic = (char*)&frameMagic;
+    char rbuff[16];
+    while(isRunning) {
+        addrLen = sizeof(clientAddr);
+        int connFd = accept(sockListen, &clientAddr, &addrLen);
+        if(connFd == -1) continue;
+        log("new data stream");
+
+        int moff = 0;
+        for(;;) {
+            // check for frame magic number
+            if(moff < sizeof(uint64_t)) {
+                auto r = read(connFd, rbuff, 1);
+                if(r <= 0) {
+                    log("end of data stream");
+                    break;
+                }
+
+                if(rbuff[0] == magic[moff]) {
+                    moff++;
+                } else {
+                    log("re-syncing data stream");
+                    moff = 0;
+                }
+                continue;
+            }
+
+            // read frame data
+            auto r = read(connFd, pixBuffNext, frameSize);
+            if(r != (ssize_t) frameSize) {
+                log("end of data stream");
+                break;
+            }
+
+            flipBuffer();
+        }
+
+        close(connFd);
+    }
+    close(sockListen);
 
     return 0;
 }
@@ -102,11 +167,13 @@ void * doUdpTx(UNUSED void *obj) {
 
     while(isRunning) {
         // transmit frame data
+        pthread_mutex_lock(&mutexBuff);
         for(const auto &panel : panels) {
             if(panel.width == 0) continue;
-            sendFrame(socketUdp, frameId++, microtime() + propagationDelay, panel);
+            sendFrame(socketUdp, frameId, microtime() + propagationDelay, panel);
         }
-        if(frameId == 0) frameId = 1;
+        pthread_mutex_unlock(&mutexBuff);
+        if(++frameId == 0) frameId = 1;
 
         // sleep until next update
         uint32_t now = microtime();
@@ -223,4 +290,328 @@ bool loadConfiguration(const char *fileName) {
     }
 
     return true;
+}
+
+void flipBuffer() {
+    // atomically flip pixel buffer
+    pthread_mutex_lock(&mutexBuff);
+    auto temp = pixBuffAct;
+    pixBuffAct = pixBuffNext;
+    pixBuffNext = temp;
+    pthread_mutex_unlock(&mutexBuff);
+}
+
+void labelPanels() {
+    int i = 0;
+    for(const auto &panel : panels) {
+        if(panel.width == 0) continue;
+        drawHex2(panel.xoff, panel.yoff, i++);
+    }
+
+    flipBuffer();
+}
+
+void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b) {
+    uint8_t *pixel = &(pixBuffNext[(y * width + x) * 3]);
+    pixel[0] = r;
+    pixel[1] = g;
+    pixel[2] = b;
+}
+
+void drawHex2(int xoff, int yoff, uint8_t value) {
+    uint8_t hi = (value >> 4u) & 0xfu;
+    uint8_t lo = value & 0xfu;
+
+    drawHex(xoff, yoff, hi);
+    drawHex(xoff + 11, yoff, lo);
+}
+
+char hexChars[16][16][11] = {
+        {
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "    ##    ",
+                "   ###    ",
+                "  ####    ",
+                " ## ##    ",
+                "##  ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "    ##    ",
+                "##########",
+                "##########"
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "##      ##",
+                "       ###",
+                "      ### ",
+                "     ###  ",
+                "    ###   ",
+                "   ###    ",
+                "  ###     ",
+                " ###      ",
+                "###       ",
+                "##########",
+                "##########"
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "##     ###",
+                "      ### ",
+                "   #####  ",
+                "   #####  ",
+                "      ### ",
+                "##     ###",
+                "##      ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "      ##  ",
+                "     ###  ",
+                "    ####  ",
+                "   #####  ",
+                "  ### ##  ",
+                " ###  ##  ",
+                "###   ##  ",
+                "##########",
+                "##########",
+                "      ##  ",
+                "      ##  ",
+                "      ##  ",
+                "      ##  ",
+                "      ##  ",
+                "      ##  ",
+                "      ##  "
+        },{
+                "##########",
+                "##########",
+                "##        ",
+                "##        ",
+                "####      ",
+                "  ####    ",
+                "    ####  ",
+                "     #### ",
+                "       ###",
+                "        ##",
+                "##      ##",
+                "##      ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##        ",
+                "##        ",
+                "##        ",
+                "## ####   ",
+                "########  ",
+                "####  ### ",
+                "###    ###",
+                "##      ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "##########",
+                "##########",
+                "        ##",
+                "       ###",
+                "      ### ",
+                "     ###  ",
+                "    ###   ",
+                "    ##    ",
+                "   ##     ",
+                "   ##     ",
+                "  ##      ",
+                "  ##      ",
+                " ##       ",
+                " ##       ",
+                "##        ",
+                "##        ",
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "###    ###",
+                " ###  ####",
+                "  ########",
+                "   #### ##",
+                "        ##",
+                "        ##",
+                "        ##",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##########",
+                "##########",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##"
+        },{
+                "#######   ",
+                "########  ",
+                "##    ### ",
+                "##     ###",
+                "##      ##",
+                "##     ###",
+                "##    ### ",
+                "########  ",
+                "########  ",
+                "##    ### ",
+                "##     ###",
+                "##      ##",
+                "##     ###",
+                "##    ### ",
+                "########  ",
+                "#######   "
+        },{
+                "   ####   ",
+                "  ######  ",
+                " ###  ### ",
+                "###    ###",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "###    ###",
+                " ###  ### ",
+                "  ######  ",
+                "   ####   "
+        },{
+                "#######   ",
+                "########  ",
+                "##    ### ",
+                "##     ###",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##      ##",
+                "##     ###",
+                "##    ### ",
+                "########  ",
+                "#######   "
+        },{
+                "##########",
+                "##########",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "########  ",
+                "########  ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##########",
+                "##########"
+        },{
+                "##########",
+                "##########",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "########  ",
+                "########  ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        ",
+                "##        "
+        }
+};
+
+void drawHex(int xoff, int yoff, uint8_t value) {
+    auto pattern = hexChars[value & 0xfu];
+
+    int x = 0;
+    for(int y = 0; y < 16; y++) {
+        for(x = 0; x < 11; x++) {
+            if (pattern[y][x] == '#')
+                setPixel(x + xoff, y + yoff, 255, 255, 0);
+            else
+                setPixel(x + xoff, y + yoff, 0, 0, 0);
+        }
+    }
 }

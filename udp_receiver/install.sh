e9202f522de5e273deeb9089efa9b4813ed13441
Robert J. Rouquette <robert.rouquette@focusvq.com>
2019-07-28 21:40:36 -0500

    fix font loading for service

--- udp_receiver/install.sh
+++ udp_receiver/install.sh
@@ -2,6 +2,7 @@
 
 cp --remove-destination -v dist/udp_receiver /usr/local/bin/rgb_matrix_udp_receiver
 cp --remove-destination -v ../rpi-rgb-led-matrix/lib/librgbmatrix.so.1 /usr/local/lib/
+cp --remove-destination -v 5x8.bdf /usr/local/share/
 cp --remove-destination -v rgb-matrix-udp-receiver.service /etc/systemd/system/
 
 systemctl daemon-reload

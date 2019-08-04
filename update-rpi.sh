835ee6d2891b5dc5ebcf65b8f79d3e2a575c2dd6
Robert J. Rouquette <robert.rouquette@focusvq.com>
2019-08-03 23:57:15 -0500

    remove override nanosleep() with clock_nanosleep()

--- update-rpi.sh
+++ update-rpi.sh
@@ -6,7 +6,7 @@ git pull
 git submodule update --remote --merge
 
 cd rpi-rgb-led-matrix/
-export USER_DEFINES="-include $(pwd)/../nanosleep.h"
+#export USER_DEFINES="-include $(pwd)/../nanosleep.h"
 make
 
 cd ../udp_receiver/

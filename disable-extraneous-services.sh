bbbe1ac8f2038cc69a65d46ac723252fcdc130ba
Robert J. Rouquette <robert.rouquette@focusvq.com>
2019-08-03 20:34:26 -0500

    add script to disable unnecessary services

---
+++ disable-extraneous-services.sh
@@ -0,0 +1,15 @@
+# disable apt updates
+sudo systemctl stop apt-daily.timer
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily.service
+sudo systemctl stop apt-daily-upgrade.timer
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl disable apt-daily-upgrade.service
+
+# disable irqbalance
+sudo systemcl stop irqbalance
+sudo systemcl disable irqbalance
+
+# disable wpa supplicant
+sudo systemctl stop wpa_supplicant
+sudo systemctl disable wpa_supplicant

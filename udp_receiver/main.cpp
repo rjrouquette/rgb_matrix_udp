7e9ba5478cf8cf85b00c13d61ab085bf7ddcfa1b
Robert J. Rouquette <robert.rouquette@focusvq.com>
2019-08-03 21:11:04 -0500

    slower gpio update rate (set to max)

--- udp_receiver/main.cpp
+++ udp_receiver/main.cpp
@@ -119,7 +119,7 @@ int main(int argc, char **argv) {
     matrix_options.pwm_bits = 11;
     matrix_options.hardware_mapping = "adafruit-hat-pwm";
 
-    runtime_opt.gpio_slowdown = 7;
+    runtime_opt.gpio_slowdown = 5;
 
     matrix = rgb_matrix::CreateMatrixFromOptions(
             matrix_options,

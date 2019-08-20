c7d037ac21b4a3d5f62ebfd2c1dd262af63d74e6
Robert J. Rouquette <robert.rouquette@focusvq.com>
2019-08-19 21:33:57 -0500

    add debug prints

--- udp_receiver/main2.cpp
+++ udp_receiver/main2.cpp
@@ -5,10 +5,16 @@
 #include "MatrixDriver.h"
 #include <cstdlib>
 #include <unistd.h>
+#include <iostream>
 
 int main(int argc, char **argv) {
+    std::cout << "initializing rgb matrix driver" << std::endl;
     MatrixDriver matrixDriver(MatrixDriver::gpio_rpi3, 64, 64, 11);
 
+    std::cout << "initialize pwm mapping" << std::endl;
+    createPwmLutCie1931(11, 20, matrixDriver.getPwmMapping());
+
+    std::cout << "sending first frame" << std::endl;
     matrixDriver.flipBuffer();
 
     return 0;

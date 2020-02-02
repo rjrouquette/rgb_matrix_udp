92f8a7abe5f93af6dedc38dd1f961c5cfa30667e
Robert J. Rouquette <robert.rouquette@focusvq.com>
2020-02-02 15:54:14 -0600

    debugging timing

--- avr/main.c
+++ avr/main.c
@@ -60,6 +60,7 @@ ISR(OSC_OSCF_vect) {
 // hsync leading edge
 ISR(PORTE_INT0_vect, ISR_NAKED) {
     uint8_t buffer[8];
+    ledToggle();
     buffer[0] = META_PORT.IN;
     buffer[1] = META_PORT.IN;
     buffer[2] = META_PORT.IN;

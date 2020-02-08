36d0769d60bb0b6b4fe02d9dc72f0d6d96c4a893
Robert J. Rouquette <robert.rouquette@focusvq.com>
2020-02-07 21:55:38 -0600

    qiangli Q3E panel

--- avr/main.c
+++ avr/main.c
@@ -59,21 +59,17 @@ ISR(OSC_OSCF_vect) {
 
 // hsync leading edge
 ISR(PORTE_INT0_vect, ISR_NAKED) {
-    uint8_t buffer[6];
+    uint8_t buffer[4];
     buffer[0] = META_PORT.IN;
     buffer[1] = META_PORT.IN;
     buffer[2] = META_PORT.IN;
     buffer[3] = META_PORT.IN;
-    buffer[3] = META_PORT.IN;
-    buffer[4] = META_PORT.IN;
     ledToggle();
 
     ROWSEL_PORT.OUT = buffer[0];
     ROWSEL_PORT.OUT = buffer[1];
-    ROWSEL_PORT.OUT = buffer[2];
-    ROWSEL_PORT.OUT = buffer[3];
-    PWM_TIMER.CCAL = buffer[4];
-    PWM_TIMER.CCAH = buffer[5];
+    PWM_TIMER.CCAL = buffer[2];
+    PWM_TIMER.CCAH = buffer[3];
     PWM_TIMER.CNTL = 0;
     PWM_TIMER.CNTH = 0;
 

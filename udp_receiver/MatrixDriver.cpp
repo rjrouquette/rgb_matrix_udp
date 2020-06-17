5791bced02ee1e61ae0c4942b385ff5f419fe805
Robert J. Rouquette <robert.rouquette@focusvq.com>
2020-06-17 15:51:27 -0500

    fix bug in RemapInterleaved32A

--- udp_receiver/MatrixDriver.cpp
+++ udp_receiver/MatrixDriver.cpp
@@ -716,9 +716,9 @@ static void die(const char *format, ...) {
 // interleaved 32 pixel wide panels
 void RemapInterleaved32A::remap(unsigned &x, unsigned &y) {
     // determine interleaving offset
-    auto offset = y & 1u;
-    // truncate y-coordinate
-    y >>= 1u;
+    auto offset = y & 8u;
+    // interleave y-coordinate
+    y = (y & 0x7u) | ((y & 0xfffffff0u) >> 1u);
     // interleave x-coordinate: leave lower 5 bits intact, shift upper bits, and add offset
-    x = (x & 0x1fu) | ((x & 0x7fffffe0u) << 1u) | (offset << 5u);
+    x = (x & 0x1fu) | ((x & 0x7fffffe0u) << 1u) | (offset << 2u);
 }
